4604c27cb421deb6c18deb277e983439800172af
Benedict Aaron Umiten <69704628+aaronbened@users.noreply.github.com>
2022-09-30 09:26:10 +0800

    Add files via upload

---
+++ new1.php
@@ -0,0 +1,32 @@
+<html>
+<head><title> This is my First Webpage </title></head>
+<body bgcolor = "Blue">
+<p> This HTML will get delieverd as is </p>
+<?php echo "<p> but this code interpreted by PHP and tuned into HTML </p>";
+
+	$username ="Umiten";
+	echo $username;
+	echo "<br>";
+	$count = 17;
+	echo "<br>";
+	echo $count;
+	echo "<br>";
+	$sum = 6+2;
+	echo $sum;
+	echo "<br>";
+	echo "My name is " .$username;
+	echo "<br>";
+	$pi = "3.14";
+	echo $pi;
+	echo "<br>";
+	$radius = 5;
+	echo $radius;
+	echo "<br>";
+	"input radius value";
+	$circle = $pi * $radius**2;
+	echo "the circle area is " .$circle;
+// this is the snippet for php code
+?>
+
+</body>
+</html>
