939b6223a8eb6247922e30fb76e036d5c48484f4
Benedict Aaron Umiten <69704628+aaronbened@users.noreply.github.com>
2022-09-30 21:47:01 +0800

    Add files via upload

---
+++ Exercise1.php
@@ -0,0 +1,15 @@
+<html>
+<head><title>Exercise 1 </title></head>
+<body bgcolor = "PINK">
+<?php 
+	$Fullname = " Benedict Aaron J. Umiten" ;
+	$CollegeProg = "College of Engineering BS Computer Engineering" ;
+	$YearAndSection = "Third Year 301" ;
+	$String = "<h1 align = 'center'>Today, I will learn PHP Programming</h1>";
+	echo "Hi my name is" .$Fullname, "<br>";
+	echo "I am currently taking " .$CollegeProg ,"<br>";
+	echo "I am now" .$YearAndSection , "<br>";
+	echo $String;
+?>
+</body>
+</html>
